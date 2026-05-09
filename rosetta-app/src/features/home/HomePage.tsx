9ce7d132684a7358a0e9bc398bff121587bcb589
Leo Lin <4258leo@gmail.com>
2026-05-09 17:34:43 +0800

    fix: wrong icon image path

--- rosetta-app/src/features/home/HomePage.tsx
+++ rosetta-app/src/features/home/HomePage.tsx
@@ -1,7 +1,7 @@
 export function HomePage() {
   return (
     <div className="w-full h-full min-h-[80vh]  flex flex-col items-center justify-center">
-      <img src="/src-tauri/icons/128x128@2x.png" alt="Rosetta" />
+      <img src="/public/icon.png" alt="Rosetta" className="w-70 h-70" />
       <h1 className="text-2xl font-semibold mt-6 text-primary">Welcome to Rosetta!</h1>
     </div>
   );
